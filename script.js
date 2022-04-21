9b72731d82886c5bd01e451df4aceb20ceb733d9
Shisui <aca.jevdjevic@gmail.com>
2022-04-21 22:07:31 +0200

    Added grid size change feature

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let btnStart = document.createElement('div');
 document.body.appendChild(btnStart);
 btnStart.setAttribute('id','btnStart');
 
+let xByY;
+
 let btnStartText = document.createElement('p');
 btnStartText.textContent = "Change grid";
 btnStart.appendChild(btnStartText);
@@ -14,7 +16,10 @@ btnStart.addEventListener('click', (e) => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
       }
-})
+      xByY = prompt("New Grid, choose between 16 and 100: ");
+      
+      newGrid(xByY);
+});
 
 
     for (i = 0; i < 16; i++) {
@@ -36,5 +41,21 @@ container.addEventListener('mouseover', (e) => {
     }
     else
     e.target.style.background = 'red';
-})
+});
    
+
+function newGrid (xy) {
+    
+    for (i = 0; i < xy; i++) {
+        let columnDiv = document.createElement('div');
+        container.appendChild(columnDiv);
+        columnDiv.style.border = "none";
+    for (j = 0; j < xy; j++) {
+        let rowDiv = document.createElement('div');
+        columnDiv.appendChild(rowDiv);
+        rowDiv.style.borderTop = "0px";
+        rowDiv.style.borderLeft = "0px";
+        
+    }
+}
+}
